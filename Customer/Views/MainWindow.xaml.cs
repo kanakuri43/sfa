1e7227317d253874ad06ae4a5e62be6f47b8dcd2
okita <t-okita@netoffice.obisan.co.jp>
2025-06-12 08:30:06 +0900

    update

---
+++ Customer/Views/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace Customer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
