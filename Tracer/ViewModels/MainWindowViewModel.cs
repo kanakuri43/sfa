149c2c18683a971d3a1cd1ea8da8e395cd33ccff
okita <junor01@gmail.com>
2025-06-28 09:53:52 +0900

    add tracer project

---
+++ Tracer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Tracer.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Tracer";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
